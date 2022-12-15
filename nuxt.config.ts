91fcf23e54b119cfb262aea2c2d24e9ad0364ec0
Rodrigo dos Santos Fernandes <2201959@my.ipleiria.pt>
2022-12-15 15:23:00 +0000

    SEO 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,13 +12,17 @@ export default defineNuxtConfig({
     head: {
         title: 'wetheknot',
         meta: [
-          { charset: 'utf-8' },
-          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-          {
-            hid: 'wetheknot',
-            name: 'wetheknot',
-            content: 'We from wetheknot are compromised with finding the perfect balance beetween environment and clothing manufactorying, keeping sustainability in mind for every product we design.'
-          }
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {name: "title", content: 'wetheknot'},
+            {name: "keywords", content: "cloth, fashion, wetheknot, durability, environment, friendly, unisex, reuse, recycle, simplicity, portuguese clothing, portugal, roupa portuguesa, roupa, unisexo, simplicidade, casacos, jackets, jeans, calças, soap"},
+            {name: "description", content: "We from wetheknot are compromised with finding the perfect balance beetween environment and clothing manufactorying, keeping sustainability in mind for every product we design."},
+            {name: "author", content: "Rodrigo Fernandes"},
+            {
+                hid: 'wetheknot',
+                name: 'wetheknot',
+                content: 'We from wetheknot are compromised with finding the perfect balance beetween environment and clothing manufactorying, keeping sustainability in mind for every product we design.'
+            }
         ],
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
         htmlAttrs: {
